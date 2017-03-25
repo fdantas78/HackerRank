ab343ec3fa0b87914d72bf4e8c3d37b0c14a366d
Fernando Dantas <development@dantas.me>
2017-03-25 17:48:47 -0300

    Update WarmupChallenges.h

--- src/WarmupChallenges.h
+++ src/WarmupChallenges.h
@@ -8,10 +8,39 @@
 #ifndef WARMUPCHALLENGES_H_
 #define WARMUPCHALLENGES_H_
 
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
 class WarmupChallenges {
 public:
 	WarmupChallenges();
 	virtual ~WarmupChallenges();
+	
+	static int Test1(int a, int b);
+        static int Test2(int n, std::vector<int>& arr);
+        static std::string Test3(std::vector<int>& arr_a, std::vector<int>& arr_b);
+        static void Test4(void);
+        static void Test5(void);
 };
 
 #endif /* WARMUPCHALLENGES_H_ */
