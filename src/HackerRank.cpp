0c6a05d2291b4f7be3d18791ff6c3ac7ea7b72f2
Fernando Dantas <development@dantas.me>
2017-03-25 17:56:11 -0300

    Added 3 solutions

--- src/HackerRank.cpp
+++ src/HackerRank.cpp
@@ -7,9 +7,63 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include "WarmupChallenges.h"
 
 int main() {
-	cout << "!!!Hello World test!!!" << endl; // prints !!!Hello World!!!
+
+	std::cout << "Warmup Challenges" << std::endl << std::endl;
+
+	{
+		std::cout << "Solve Me First" << std::endl << "-------------------" << std::endl;
+
+		//Initialization
+		int a = 3, b = 4;
+
+		//Solution
+		std::cout << WarmupChallenges::Test1(a,b) << std::endl;
+		std::cout << "-------------------" << std::endl;
+	}
+
+	{
+		std::cout << "Simple Array Sum" << std::endl << "-------------------" << std::endl;
+
+		//Initialization
+		int n = 6;
+		std::vector<int> arr = {1, 2, 3, 4, 10, 11};
+
+		//Solution
+		std::cout << WarmupChallenges::Test2(n,arr) << std::endl;
+		std::cout << "-------------------" << std::endl;
+	}
+
+	{
+		std::cout << "Compare the Triplets" << std::endl << "-------------------" << std::endl;
+		//Initialization
+		std::vector<int> arr_a = {5, 6, 7};
+		std::vector<int> arr_b = {3, 6, 10};
+
+		//Solution
+		std::cout << WarmupChallenges::Test3(arr_a,arr_b) << std::endl;
+		std::cout << "-------------------" << std::endl;
+	}
+
+	{
+		std::cout << "Solve Me First" << std::endl << "-------------------" << std::endl;
+		//Initialization
+		//Solution
+		//std::cout << WarmupChallenges::Test4() << std::endl;
+		std::cout << "-------------------" << std::endl;
+
+	}
+
+
+	{
+		std::cout << "" << std::endl << "-------------------" << std::endl;
+		//Initialization
+		//Solution
+		//std::cout << WarmupChallenges::Test5() << std::endl;
+		std::cout << "-------------------" << std::endl;
+	}
+
 	return 0;
 }
--- src/WarmupChallenges.cpp
+++ src/WarmupChallenges.cpp
@@ -16,3 +16,44 @@ WarmupChallenges::~WarmupChallenges() {
 	// TODO Auto-generated destructor stub
 }
 
+int WarmupChallenges::Test1(int a, int b)
+{
+    return a+b;
+}
+
+int WarmupChallenges::Test2(int n, std::vector<int>& arr)
+{
+    int sum = 0;
+    for(int arr_i = 0;arr_i < n;arr_i++){
+      sum += arr[arr_i];
+    }
+
+    return sum;
+}
+
+std::string WarmupChallenges::Test3(std::vector<int>& arr_a, std::vector<int>& arr_b)
+{
+    int a_points = 0, b_points = 0;
+
+    for(unsigned int arr_i = 0;arr_i < arr_a.size();arr_i++){
+       if((arr_a[arr_i]-arr_b[arr_i]) > 0) //a win
+           a_points++;
+       else if((arr_b[arr_i]-arr_a[arr_i]) > 0) //b win
+           b_points++;
+    }
+
+    return std::to_string(a_points) + " " + std::to_string(b_points);
+}
+
+void WarmupChallenges::Test4(void)
+{
+
+}
+
+void WarmupChallenges::Test5(void)
+{
+    std::cout << "" << std::endl << "-------------------" << std::endl;
+
+    std::cout << "" << std::endl;
+    std::cout << "-------------------" << std::endl;
+}
