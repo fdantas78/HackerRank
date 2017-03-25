fb8b16d53646f6835d88e5af5f02264cbca84421
Fernando Dantas <development@dantas.me>
2017-03-25 17:47:21 -0300

    Create class to store the WarmupChallenges solutions.

---
+++ src/WarmupChallenges.cpp
@@ -0,0 +1,18 @@
+/*
+ * WarmupChallenges.cpp
+ *
+ *  Created on: Mar 25, 2017
+ *      Author: makaidaimyo
+ */
+
+#include "WarmupChallenges.h"
+
+WarmupChallenges::WarmupChallenges() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WarmupChallenges::~WarmupChallenges() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/WarmupChallenges.h
@@ -0,0 +1,17 @@
+/*
+ * WarmupChallenges.h
+ *
+ *  Created on: Mar 25, 2017
+ *      Author: makaidaimyo
+ */
+
+#ifndef WARMUPCHALLENGES_H_
+#define WARMUPCHALLENGES_H_
+
+class WarmupChallenges {
+public:
+	WarmupChallenges();
+	virtual ~WarmupChallenges();
+};
+
+#endif /* WARMUPCHALLENGES_H_ */
